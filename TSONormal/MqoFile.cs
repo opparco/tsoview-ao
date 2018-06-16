42af297d059a81986a3181e28241a7d27af92738
opparco <parco.opaai@gmail.com>
2018-06-17 00:19:17 +0900

    import TSONormal

---
+++ TSONormal/MqoFile.cs
@@ -0,0 +1,258 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using Direct3D = Microsoft.DirectX.Direct3D;
+
+namespace TDCG
+{
+    /// <summary>
+    /// 統合メッシュ
+    /// </summary>
+    public class MqoMesh
+    {
+        /// <summary>
+        /// 頂点配列
+        /// </summary>
+        public MqoVert[] vertices;
+
+        TSOMesh mesh = null;
+
+        static Dictionary<TSOMesh, MqoMesh> meshmap = new Dictionary<TSOMesh, MqoMesh>();
+
+        internal static MqoMesh FromTSOMesh(TSOMesh mesh)
+        {
+            MqoMesh mqo_mesh;
+            if (meshmap.TryGetValue(mesh, out mqo_mesh))
+                return mqo_mesh;
+
+            mqo_mesh = new MqoMesh(mesh);
+            mqo_mesh.CreateVertices();
+            meshmap[mesh] = mqo_mesh;
+            
+            return mqo_mesh;
+        }
+
+        public MqoMesh(TSOMesh mesh)
+        {
+            this.mesh = mesh;
+        }
+
+        internal void CreateVertices()
+        {
+            Heap<MqoVert> mh = new Heap<MqoVert>();
+
+            foreach (TSOSubMesh sub_mesh in mesh.sub_meshes)
+            {
+                int cnt = 0;
+                ushort a, b = 0, c = 0;
+                MqoVert ma, mb = new MqoVert(), mc = new MqoVert();
+
+                foreach (Vertex v in sub_mesh.vertices)
+                {
+                    MqoVert m = new MqoVert(v.position, v.normal, null);
+                    ushort i;
+                    if (mh.map.TryGetValue(m, out i))
+                    {
+                        //集約先はidx=i
+                        m = mh.ary[i];
+                    }
+                    else
+                    {
+                        m.skin_weights = new MqoSkinWeight[4];
+                        for (int w = 0; w < 4; w++)
+                        {
+                            m.skin_weights[w] = new MqoSkinWeight(sub_mesh.GetBone(v.skin_weights[w].bone_index), v.skin_weights[w].weight);
+                        }
+                        //mはidx=iになる
+                        i = (ushort)mh.Count;
+                        mh.Add(m);
+                    }
+                    //集約する
+                    m.rel.Add(new TSOPair(v, sub_mesh));
+
+                    cnt++;
+                    a = b;
+                    b = c;
+                    c = i;
+                    ma = mb;
+                    mb = mc;
+                    mc = m;
+
+                    //m.Index = i;
+
+                    if (cnt < 3)
+                        continue;
+
+                    if (a != b && b != c && c != a)
+                    {
+                        //todo: create MqoFace
+                    }
+                }
+            }
+            vertices = mh.ary.ToArray();
+        }
+
+        internal void CalcSkindeform(Matrix [] clipped_boneMatrices)
+        {
+            foreach (MqoVert v in vertices)
+            {
+                v.deformed_position = v.CalcSkindeformPosition(clipped_boneMatrices);
+                v.deformed_normal = v.CalcSkindeformNormal(clipped_boneMatrices);
+            }
+        }
+
+        internal void WriteBuffer()
+        {
+            foreach (TSOSubMesh sub_mesh in mesh.sub_meshes)
+                sub_mesh.WriteBuffer();
+        }
+    }
+
+    public class TSOPair
+    {
+        public Vertex a;
+        public TSOSubMesh sub_mesh;
+
+        public TSOPair(Vertex a, TSOSubMesh sub_mesh)
+        {
+            this.a = a;
+            this.sub_mesh = sub_mesh;
+        }
+    }
+
+    /// <summary>
+    /// 統合頂点
+    /// </summary>
+    public class MqoVert
+    {
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public Vector3 position;
+        /// <summary>
+        /// 法線
+        /// </summary>
+        public Vector3 normal;
+        /// <summary>
+        /// スキンウェイト配列
+        /// </summary>
+        public MqoSkinWeight[] skin_weights;
+
+        /// 選択中であるか
+        public bool selected = false;
+
+        /// ウェイト乗数
+        public float factor;
+
+        public List<TSOPair> rel = new List<TSOPair>();
+
+        public Vector3 deformed_position;
+        public Vector3 deformed_normal;
+
+        public MqoVert()
+        {
+        }
+
+        public MqoVert(Vector3 position, Vector3 normal, MqoSkinWeight[] skin_weights)
+        {
+            this.position = position;
+            this.normal = normal;
+            this.skin_weights = skin_weights;
+        }
+
+        public override int GetHashCode()
+        {
+            return position.GetHashCode() ^ normal.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is MqoVert)
+            {
+                MqoVert o = (MqoVert)obj;
+                return position.Equals(o.position) && normal.Equals(o.normal);
+            }
+            return false;
+        }
+
+        public bool Equals(MqoVert o)
+        {
+            if ((object)o == null)
+            {
+                return false;
+            }
+
+            return position.Equals(o.position) && normal.Equals(o.normal);
+        }
+
+        internal Vector3 CalcSkindeformPosition(Matrix[] bone_matrices)
+        {
+            Vector3 pos = Vector3.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                Matrix m = bone_matrices[skin_weights[i].bone.Id];
+                float w = skin_weights[i].normalized_weight;
+                pos += Vector3.TransformCoordinate(position, m) * w;
+            }
+            return pos;
+        }
+
+        internal Vector3 CalcSkindeformNormal(Matrix[] bone_matrices)
+        {
+            Vector3 nor = Vector3.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                Matrix m = bone_matrices[skin_weights[i].bone.Id];
+                m.M41 = 0;
+                m.M42 = 0;
+                m.M43 = 0;
+                float w = skin_weights[i].normalized_weight;
+                nor += Vector3.TransformCoordinate(normal, m) * w;
+            }
+            return nor;
+        }
+    }
+
+    /// <summary>
+    /// 統合スキンウェイト
+    /// </summary>
+    public class MqoSkinWeight : IComparable
+    {
+        /// <summary>
+        /// node (bone)
+        /// </summary>
+        public TSONode bone;
+
+        /// <summary>
+        /// ウェイト
+        /// </summary>
+        public float weight;
+
+        /// <summary>
+        /// 正規化済みウェイト
+        /// </summary>
+        public float normalized_weight;
+
+        public MqoSkinWeight(TSONode bone, float weight)
+        {
+            this.bone = bone;
+            this.weight = weight;
+            this.normalized_weight = weight;
+        }
+
+        /// <summary>
+        /// 比較関数
+        /// </summary>
+        /// <param name="obj">比較対象スキンウェイト</param>
+        /// <returns>比較結果</returns>
+        public int CompareTo(object obj)
+        {
+            return -weight.CompareTo(((MqoSkinWeight)obj).weight);
+        }
+    }
+}
---
+++ TSONormal/Program.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using Direct3D = Microsoft.DirectX.Direct3D;
+using TDCG;
+
+namespace TSONormal
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("TSONormal.exe <tso file> <tmo file> [fig file]");
+                return;
+            }
+            string tso_file = args[0];
+            string tmo_file = args[1];
+
+            string fig_file = null;
+            if (args.Length > 2)
+            {
+                fig_file = args[2];
+            }
+
+            TSOFile tso = new TSOFile();
+            tso.Load(tso_file);
+
+            TMOFile tmo = new TMOFile();
+            tmo.Load(tmo_file);
+
+            Figure fig = new Figure();
+            fig.TsoList.Add(tso);
+            fig.Tmo = tmo;
+
+            if (fig_file != null)
+            {
+                List<float> ratios = ReadFloats(fig_file);
+                /*
+                ◆FIGU
+                スライダの位置。値は float型で 0.0 .. 1.0
+                    0: 姉妹
+                    1: うで
+                    2: あし
+                    3: 胴まわり
+                    4: おっぱい
+                    5: つり目たれ目
+                    6: やわらか
+                 */
+                SliderMatrix slider_matrix = fig.SliderMatrix;
+                slider_matrix.AgeRatio = ratios[0];
+                slider_matrix.ArmRatio = ratios[1];
+                slider_matrix.LegRatio = ratios[2];
+                slider_matrix.WaistRatio = ratios[3];
+                slider_matrix.OppaiRatio = ratios[4];
+                slider_matrix.EyeRatio = ratios[5];
+            }
+            fig.UpdateNodeMap();
+            if (fig_file != null)
+                fig.UpdateBoneMatrices();
+            else
+                fig.UpdateBoneMatricesWithoutSlider(true);
+
+            foreach (TSOMesh mesh in tso.meshes)
+                foreach (TSOSubMesh sub in mesh.sub_meshes)
+                {
+                    Matrix[] clipped_bone_matrices = fig.ClipBoneMatrices(sub);
+
+                    for (int i = 0; i < sub.vertices.Length; i++)
+                    {
+                        CalcSkindeform(ref sub.vertices[i], clipped_bone_matrices);
+                    }
+                }
+
+            foreach (TSONode tso_node in tso.nodes)
+            {
+                TMONode tmo_node;
+                if (fig.nodemap.TryGetValue(tso_node, out tmo_node))
+                    tso_node.TransformationMatrix = tmo_node.TransformationMatrix;
+            }
+
+            tso.Save(@"out.tso");
+        }
+
+        public static void CalcSkindeform(ref Vertex v, Matrix[] bone_matrices)
+        {
+            Vector3 pos = Vector3.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                Matrix m = bone_matrices[v.skin_weights[i].bone_index];
+                float w = v.skin_weights[i].weight;
+                pos += Vector3.TransformCoordinate(v.position, m) * w;
+            }
+            v.position = pos;
+
+            Vector3 nor = Vector3.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                Matrix m = bone_matrices[v.skin_weights[i].bone_index];
+                m.M41 = 0;
+                m.M42 = 0;
+                m.M43 = 0;
+                float w = v.skin_weights[i].weight;
+                nor += Vector3.TransformCoordinate(v.normal, m) * w;
+            }
+            v.normal = Vector3.Normalize(nor);
+        }
+
+        static List<float> ReadFloats(string dest_file)
+        {
+            List<float> floats = new List<float>();
+            string line;
+            using (StreamReader source = new StreamReader(File.OpenRead(dest_file)))
+                while ((line = source.ReadLine()) != null)
+                {
+                    floats.Add(Single.Parse(line));
+                }
+            return floats;
+        }
+    }
+}
