83e49e43b2820518f1473fa2154df16817127e46
opparco <parco.opaai@gmail.com>
2019-11-16 17:11:50 +0900

    remove unused using

--- TDCG/Figure.cs
+++ TDCG/Figure.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-//using System.Drawing;
-using System.Threading;
-//using System.ComponentModel;
 using System.IO;
 
 using Microsoft.DirectX;
